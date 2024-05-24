1214e4f78f55600737fc10b7c21e50b822b26cdc
DebdipWritesCode <debdipmukherjee52@gmail.com>
2024-05-24 21:18:14 +0530

    solve cors error

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const movieRoutes = require('./routes/movie');
+const corsOptions = {
+  origin: "http://your-frontend-domain.com",
+  optionsSuccessStatus: 200, 
+};
+
+app.use(cors(corsOptions));
+
+const movieRoutes = require("./routes/movie");
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
 app.use(bodyParser.json());
-app.use('/movie', movieRoutes);
+app.use("/movie", movieRoutes);
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+  console.log("Server is running on port 3000");
+});
